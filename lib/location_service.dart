6603c5a1ad8c971139eeb2ceeb63f06a94d439d9
FaizaFaisal <76447366+FaizaFaisal@users.noreply.github.com>
2023-11-04 21:48:43 +0300

    Update location_service.dart

--- lib/location_service.dart
+++ lib/location_service.dart
@@ -5,7 +5,7 @@ Future<http.Response> getLocationData(String text) async {
   http.Response response;
   response = await http.get(
     Uri.parse(
-        "http://mvs.bslmeiyu.com/api/v1/config/place-api-autocomplete?search_text=$text"),
+        "http://""?search_text=$text"),
     headers: {"Content-Type": "application/json"},
   );
   print(jsonDecode(response.body));
